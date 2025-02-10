98abcb2412fb6afca920c0d0c044627ec3108115
Jacob Valore <jvalore@adcogov.org>
2025-02-10 15:18:24 -0700

    added user ID to ManageUsers page

--- app/javascript/components/ManageUsers/List.js
+++ app/javascript/components/ManageUsers/List.js
@@ -36,6 +36,12 @@ const List = (props) => {
                   <tr>
                     <th
                       scope="col"
+                      className="px-1 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      ID
+                    </th>
+                    <th
+                      scope="col"
                       className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                     >
                       User
